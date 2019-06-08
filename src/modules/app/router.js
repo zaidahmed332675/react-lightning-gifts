a1b07d2815780070eebf16324dce9a22f23eef18
Ross Dyson <dyson.ross@gmail.com>
2019-06-08 16:05:04 +0900

    landing page wip

--- src/modules/app/router.js
+++ src/modules/app/router.js
@@ -4,13 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 
 // Module Dependencies
 import NotFoundPage from 'modules/error/pages/not-found';
+import LandingPage from 'modules/create/pages/landing-page';
 
 /**
  * RouterMap
  */
 const RouterMap = () => (
     <Switch>
-        <Route exact path="/" component={null} />
+        <Route exact path="/" component={LandingPage} />
         <Route component={NotFoundPage} />
     </Switch>
 );
---
+++ src/modules/create/pages/landing-page.js
@@ -0,0 +1,100 @@
+// NPM Dependencies
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import _ from 'lodash';
+import { withRouter } from 'react-router-dom';
+
+// UI Dependencies
+import { Spin, Form, Input, Button, Row, Col } from 'antd';
+
+// Local Dependencies
+import { createProjectSignal } from '../actions';
+
+class LandingPage extends Component {
+    static propTypes = {
+        form: PropTypes.shape({
+            getFieldDecorator: PropTypes.func.isRequired,
+            validateFields: PropTypes.func.isRequired
+        }).isRequired,
+        history: PropTypes.object.isRequired,
+        // createProject: PropTypes.func.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false
+        };
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { createProject, form, history } = this.props;
+
+        form.validateFields((err, values) => {
+            if (!err) {
+                const { repoUrl } = values;
+                let path = url.parse(repoUrl).pathname;
+
+                if (_.endsWith(path, '/')) {
+                    path = path.slice(0, -1);
+                }
+
+                createProject({ path }, { history });
+            }
+        });
+    };
+
+    render() {
+        const { loading } = this.state;
+        const { getFieldDecorator } = this.props.form;
+
+        if (loading) {
+            return (
+                <div style={{ textAlign: 'center' }}>
+                    <Spin tip="loading..." size="large" style={{ marginTop: '200px' }} />
+                </div>
+            );
+        }
+
+        return (
+            <Row type="flex" align="middle" justify="center" style={{ height: '100%' }}>
+                <Col style={{ textAlign: 'center' }}>
+                    <h2 style={{ marginBottom: 20 }}>First, enter your repo github URL</h2>
+                    {/*<Form onSubmit={this.handleSubmit} layout="vertical" hideRequiredMark>*/}
+                    {/*    <Form.Item>*/}
+                    {/*        {getFieldDecorator('repoUrl', {*/}
+                    {/*            rules: [{ required: true, message: 'Repo required' }]*/}
+                    {/*        })(<Input placeholder="Enter repo URL" />)}*/}
+                    {/*    </Form.Item>*/}
+                    {/*    <Form.Item>*/}
+                    {/*        <Button type="primary" htmlType="submit">*/}
+                    {/*            Create*/}
+                    {/*        </Button>*/}
+                    {/*    </Form.Item>*/}
+                    {/*</Form>*/}
+                </Col>
+            </Row>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        ...state
+        // droneOnline: state.cockpit.droneOnline,
+        // cockpitLoading: state.cockpit.cockpitLoading
+    };
+};
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({
+        // createProject: createProjectSignal.request
+    }, dispatch);
+
+const WrappedLandingPage = Form.create()(LandingPage);
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(WrappedLandingPage));
