5d4d08626bc51b31be914856265a2029b27e5484
Ross Dyson <dyson.ross@gmail.com>
2019-06-29 22:23:38 +0900

    prod api URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 const { NODE_ENV } = process.env;
 
 export const APP_NAME = 'Lightning Gifts';
-// export const SERVER_URL = 'https://tk0rn.sse.codesandbox.io';
+// export const SERVER_URL = 'https://api.lightning.gifts';
 export const SERVER_URL = NODE_ENV === 'production' ?
-    'https://node-lightning-in-a-box.herokuapp.com' : 'https://tk0rn.sse.codesandbox.io';
+    'https://api.lightning.gifts' : 'https://tk0rn.sse.codesandbox.io';
 
