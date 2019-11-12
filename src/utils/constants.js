177395590c9d37aac2f328aecde7638953de98c5
Ross <me@rossdyson.com>
2019-11-12 14:42:19 +0900

    new build config shit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,9 @@ const { NODE_ENV } = process.env;
 
 export const APP_NAME = 'Lightning Gifts';
 // export const SERVER_URL = 'https://4328b916.ngrok.io';
-export const SERVER_URL = NODE_ENV === 'production' ?
-    'https://api.lightning.gifts' : 'http://localhost:8080';
+export const ENV_URLS = {
+    production: 'https://api.lightning.gifts',
+    staging: 'https://node-lightning-gifts-staging.herokuapp.com',
+    development: 'http://localhost:8080'
+};
+export const SERVER_URL = ENV_URLS[NODE_ENV] !== undefined ? ENV_URLS[NODE_ENV] : ENV_URLS.staging;
