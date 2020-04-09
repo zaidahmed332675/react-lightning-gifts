4245fb31f1a26c6fd80a9e1888019b26b1996e62
Ross <me@rossdyson.com>
2020-04-09 22:32:30 +0900

    lnurl pay tab

--- src/modules/create/components/create-box.js
+++ src/modules/create/components/create-box.js
@@ -1,18 +1,13 @@
 // NPM Dependencies
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import QRCode from 'qrcode.react';
-import bech32 from 'bech32';
 
 // UI Dependencies
 import { Card, Col, Row } from 'antd';
 
-// Util Dependencies
-import InputCopyButton from 'utils/components/input-copy-button';
-
 // Local Dependencies
-import { SERVER_URL } from 'utils/constants';
 import CreateForm from '../forms/create-form';
+import LnurlPayTab from './lnurl-pay-tab';
 
 class CreateBox extends Component {
     static propTypes = {
@@ -35,49 +30,13 @@ class CreateBox extends Component {
         const { activeTab } = this.state;
         const { toggleModal } = this.props;
 
-        const lnurlpay = bech32.encode('lnurl',
-            bech32.toWords(Buffer.from(`${SERVER_URL}/lnurl`)),
-            1500
-        );
-
-
         let tabContent = null;
         switch (activeTab) {
             case 'basic':
                 tabContent = <CreateForm toggleModal={toggleModal} />;
                 break;
-            case 'advanced':
-                tabContent = (
-                    <>
-                        <p><b>Advanced features with LN Gifts API:</b></p>
-                        <ul style={{ textAlign: 'left' }}>
-                            <li>Generate and redeem gifts programmatically</li>
-                            <li>Get notified when someone redeems your gift with webhooks</li>
-                            <li>Add a security code to your gifts</li>
-                        </ul>
-                        <p>
-                            <a href="https://docs.lightning.gifts" target="_blank" rel="noopener noreferrer">
-                                Go to API Docs
-                            </a>
-                        </p>
-                    </>
-                );
-                break;
             case 'lnurl':
-                tabContent = (
-                    <>
-                        <p>Scan the following code to <b>create gifts instantly right from your wallet:</b></p>
-                        <a href={`lightning:${lnurlpay}`}>
-                            <QRCode
-                                value={lnurlpay}
-                                size={128}
-                                style={{ marginBottom: 12 }}
-                                renderAs="svg"
-                            />
-                        </a>
-                        <InputCopyButton text={lnurlpay} />
-                    </>
-                );
+                tabContent = <LnurlPayTab />;
                 break;
             default:
                 break;
@@ -87,7 +46,7 @@ class CreateBox extends Component {
             <Card
                 className="create-box"
                 style={{ width: '100%' }}
-                tabList={[{ key: 'basic', tab: 'Basic' }, { key: 'advanced', tab: 'Advanced' }, { key: 'lnurl', tab: 'LNURL' }]}
+                tabList={[{ key: 'basic', tab: 'Basic' }, { key: 'lnurl', tab: 'LNURL' }]}
                 activeTabKey={activeTab}
                 onTabChange={(key) => this.onTabChange(key)}
             >
